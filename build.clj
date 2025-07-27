f356c2f4b16e2507322e005a9fa030d38c12b2a3
Bill Hedworth <bill.hedworth@gmail.com>
2025-07-27 12:45:58 +0100

    first

---
+++ build.clj
@@ -0,0 +1,35 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'com.bigsy/hl7v2-stub)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis (b/create-basis {:project "deps.edn"})
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/Bigsy/hl7v2-stub"
+                      :connection "scm:git:git://github.com/Bigsy/hl7v2-stub.git"
+                      :developerConnection "scm:git:ssh://git@github.com/Bigsy/hl7v2-stub.git"
+                      :tag (str "v" version)}})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [_]
+  (jar nil)
+  (try 
+    ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+     {:installer :remote
+      :artifact (b/resolve-path jar-file)
+      :pom-file (b/pom-path {:lib lib :class-dir class-dir})})
+    (catch Exception e
+      (throw (ex-info "Deployment failed" {:lib lib :version version} e)))))
---
+++ src/hl7v2_stub/core.clj
@@ -0,0 +1,6 @@
+(ns hl7v2-stub.core)
+
+(defn hello
+  "Example function that greets someone."
+  [name]
+  (str "Hello, " name "!"))
